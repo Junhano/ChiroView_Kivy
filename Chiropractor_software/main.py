623d85d2fedc5501c929c61d83a917c812616280
Junhan Ouyang <50847334+Junhano@users.noreply.github.com>
2020-02-14 02:32:02 -0800

    Update main.py

--- Chiropractor_software/main.py
+++ Chiropractor_software/main.py
@@ -3,6 +3,9 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, NoTransition
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
+from kivy.uix.popup import Popup
+import smtplib
+
 
 class HomeScreen(Screen):
     pass
@@ -19,6 +22,9 @@ class ThirdScreen(Screen):
 class BugSetting(Screen):
     pass
 
+class BugSending(Screen):
+    pass
+
 
 GUI = Builder.load_file("chiro.kv")
 
@@ -30,5 +36,28 @@ class MainApp(App):
         screen_manager = self.root.ids['screen_manager']
         screen_manager.transition = NoTransition()
         screen_manager.current = screen_name 
+    
+    def show_popup(self, logic_bool = True):
+        if logic_bool:
+            show = BugSending()
+            global popupWindow
+            popupWindow = Popup(title = "Bug sending", content = show, size_hint = (None,None), size = (400,400))
+            popupWindow.open()
+        else:
+            popupWindow.dismiss()
+            
+    def send_message(self, text):
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.ehlo()
+            server.starttls()
+            server.login('jimmyouyangca@gmail.com','20000620oyjhwork')
+            server.sendmail('jimmyouyangca@gmail.com','junhano@uci.edu',text)
+        except:
+            print('Something went wrong')
+        finally:
+            popupWindow.dismiss()
+        
 
+        
 MainApp().run()
