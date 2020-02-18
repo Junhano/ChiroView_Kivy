c10f8d1f501d13f0467f8dfe3dfc6f706ff977cb
Junhan Ouyang <50847334+Junhano@users.noreply.github.com>
2020-02-17 21:51:55 -0800

    Update main.py

--- Chiropractor_software/main.py
+++ Chiropractor_software/main.py
@@ -4,6 +4,7 @@ from kivy.uix.screenmanager import Screen, NoTransition
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
 from kivy.uix.popup import Popup
+from kivy.graphics import Line, Color
 import smtplib
 
 
@@ -51,13 +52,35 @@ class MainApp(App):
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.ehlo()
             server.starttls()
-            server.login('jimmyouyangca@gmail.com','20000620oyjhwork')
-            server.sendmail('jimmyouyangca@gmail.com','junhano@uci.edu',text)
+            server.login('jimmybugreport@gmail.com','20000620oyjhwork')
+            server.sendmail('jimmybugreport@gmail.com','junhano@uci.edu',text)
         except:
             print('Something went wrong')
         finally:
             popupWindow.dismiss()
         
+
+    def modify_image(self,vertical,horizontal,objectname):
+        global remove_list
+        remove_list = []
+        try:
+            vertical = int(vertical)
+            horizontal = int(horizontal)
+            with objectname.canvas:
+                Color(0, 0, 0)
+                for k in range(vertical):
+                    temp = Line(points=[objectname.x + objectname.width // (vertical + 1) * (k+1), objectname.y, objectname.x + objectname.width // (vertical + 1) * (k+1), objectname.y + objectname.height], width=1.2)
+                    remove_list.append(temp)
+                for i in range(horizontal):
+                    temp = Line(points=[objectname.x, objectname.y + (objectname.height //(horizontal + 1) * (i + 1)), objectname.x + objectname.width, objectname.y + (objectname.height // (horizontal + 1) * (i+1))], width=1.2)
+                    remove_list.append(temp)
+        except:
+            print("Error in getting num lines")
+    
+    def clean_image(self,objectname):
+        for i in remove_list:
+            objectname.canvas.remove(i)
+        remove_list.clear()
 
         
 MainApp().run()
